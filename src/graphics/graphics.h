23b276df7cfe2de740c6d8cb8678bcdc4a7290b9
riemervdzee <riemervdzee@gmail.com>
2012-03-08 13:23:34 +0100

    Put the windowing code in a different file @ graphics-backend

---
+++ src/graphics/graphics.h
@@ -0,0 +1,15 @@
+/*
+ * graphics.h
+ *
+ *  Created on: 8 mrt. 2012
+ *      Author: riemer
+ */
+
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+#include "gVideo.h"
+#include "gInput.h"
+#include "gWindow.h"
+
+#endif /* GRAPH_H_ */
